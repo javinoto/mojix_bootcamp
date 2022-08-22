1d06f03cd13c23e324cb565395c6e9cb5d7af15c
Javier Hugo Noto <javiernoto93@gmail.com>
2022-08-22 00:00:07 -0400

    Quitamos 2 librerias

--- bootcamp_my_first_app.py
+++ bootcamp_my_first_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
-import seaborn as sns
-import matplotlib.pyplot as plt
+# import seaborn as sns
+# import matplotlib.pyplot as plt
 
 st.title('CSV Reader')
 file = st.file_uploader('Upload a csv', type = "csv")
